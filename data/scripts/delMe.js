b1a6d27268404b480c9194efc07a073505dc3963
Ragavan Govindarajan <ragavanspd@gmail.com>
2011-12-10 13:35:26 +0530

    saving functionData to a file in profiles

--- data/scripts/delMe.js
+++ data/scripts/delMe.js
@@ -1,23 +1,27 @@
 var rulesStruct = {
- rules: [
-  {
-  name: 'rule1',
-  shortcuts:[{key1:'a',key2:'b',matchers:'sadf,sdfasd',shortcutFunction:'functionName',cache:false},
-			{key1:'a',key2:'b',matchers:'sadf,sdfasd',shortcutFunction:'functionName',cache:true}],
-  applyTo: ['*google.com*','*meow.com*'],
-  dontApplyTo:['*evilSite.com*', '*someRandomSite.com*']
- },
- {
-  name: 'rule2',
-  shortcuts:[{key1:'a',key2:'b',matchers:'sadf,sdfasd',shortcutFunction:'functionName'},
-			{key1:'a',key2:'b',matchers:'sadf,sdfasd',shortcutFunction:'functionName'}],
-  applyTo: [],
-  dontApplyTo:[]
- }
- ],
+    rules: [
+        {
+            name: 'rule1',
+            shortcuts:[
+                {key1:'a',key2:'b',matchers:'sadf,sdfasd',shortcutFunction:'functionName',cache:false},
+                {key1:'a',key2:'b',matchers:'sadf,sdfasd',shortcutFunction:'functionName',cache:true}
+            ],
+            applyTo: ['*google.com*','*meow.com*'],
+            dontApplyTo:['*evilSite.com*', '*someRandomSite.com*']
+        },
+        {
+            name: 'rule2',
+            shortcuts:[
+                {key1:'a',key2:'b',matchers:'sadf,sdfasd',shortcutFunction:'functionName',cache:true},
+                {key1:'a',key2:'b',matchers:'sadf,sdfasd',shortcutFunction:'functionName',cache:true}
+            ],
+            applyTo: [],
+            dontApplyTo:[]
+        }
+    ],
 
- functionData:
-			{
-			 functionName: 'function(matches){this is how a function goes as string}'
-			}
+    functionData:
+    {
+        functionName: 'function(matches){this is how a function goes as string}'
+    }
 };
