b6e9287a1ebeabf2323b00ad2945f1bccd378b75
Ragavan Govindarajan <ragavanspd@gmail.com>
2012-12-29 11:14:27 +0530

    what are omnisequences, where to get.

---
+++ config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/omnisequences/"
+http_images_path = "/omnisequences/images"
+http_fonts_path = "/omnisequences/fonts"
+css_dir = "public/omnisequences/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
